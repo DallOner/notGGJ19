f334cad9f5a6fd155e719c5a674ed83ed7ab2b13
jorge <jbousing@gmail.com>
2019-01-26 12:58:22 -0400

    cambios nuevos

--- scenes/player2.gd
+++ scenes/player2.gd
@@ -5,23 +5,23 @@ const gravity = 20
 const jumpHeight = -500
 const speed = 5
 var limite
-var motion = Vector2()
 
 func _ready():
 	limite = get_viewport_rect().size
 
+var motion = Vector2()
 
 func _physics_process(delta):
 	motion.y += gravity
 	
-	if Input.is_key_pressed(KEY_D):
+	if Input.is_action_pressed("ui_right"):
 		motion.x += speed
-	if Input.is_key_pressed(KEY_A):
+	if Input.is_action_pressed("ui_left"):
 		motion.x -= speed	 
 	else:
 		motion.x += 0
 	if is_on_floor():
-		if Input.is_key_pressed(KEY_W):
+		if Input.is_action_pressed("ui_up"):
 			motion.y = jumpHeight
 	
 	motion = move_and_slide(motion,up)
