4de3900bcb03553b88aec1bd11ed233d1db8044c
jorge <jbousing@gmail.com>
2019-01-26 15:04:08 -0400

    funcionalidad para aparecer plataformas

---
+++ scenes/Mundo.gd
@@ -0,0 +1,21 @@
+extends Node
+
+const platforms = preload("res://scenes/plataformaSPAWN.tscn")
+var screen = OS.get_real_window_size()
+var numPlatforms
+
+func _ready():
+	
+	for i in range(rand_range(4,10)):
+		var plataforma = platforms.instance()
+		var pos = Vector2()
+		pos.x = rand_range(0,screen.x)
+		pos.y = rand_range(0,screen.y)
+		plataforma.position = pos
+		$platformsContainer.add_child(plataforma)	
+	
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
